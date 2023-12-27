f8d194e3270e791cc22f370b8f2450337913b7cf
김승범 <kimsb7218@naver.com>
2023-12-27 22:29:14 +0900

    Initial commit

---
+++ src/CommentJava.java
@@ -0,0 +1,20 @@
+ /*
+        소스 코드가 복잡하다면 소스 코드에 대한 이해를 돕기 위해 설명을 적어두고 싶을 수 있다.
+        또는 특정 코드를 지우지 않고, 잠시 실행을 막아두고 싶을 때도 있다.
+        이럴 때 주석을 사용하면 된다. 자바는 주석이 있는 곳을 무시한다
+        주석은 쉽게 이야기해서 자바 프로그램이 읽지 않고 무시하는 부분, 사람이 읽기 위해서 사용
+ */
+
+
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java 1"); // hello java1을 출력합니다.
+        System.out.println("hello java 2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
